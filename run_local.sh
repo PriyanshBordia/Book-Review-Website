5a8358c9fa9d26adc3be39e28b9ff1a3c9640eea
Priyansh Bordia <priyanshbordia1@gmail.com>
2020-04-28 19:50:20 +0530

    files to run app locally

---
+++ run_local.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export FLASK_APP=application.py
+export DATABASE_URL=postgres://bezoopliubauew:ff447efa87a96336c27fa8c83def232d790d8b48f8da38e236259b4f447926f7@ec2-46-137-84-140.eu-west-1.compute.amazonaws.com:5432/d9kk3aqpv4nur1
+flask run
