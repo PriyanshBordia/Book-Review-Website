06515b47b19832a07158088b0a0b8b7089040ed8
Priyansh Bordia <priyanshbordia1@gmail.com>
2020-04-29 16:34:46 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,14 @@
 import os
 import requests
 
-from flask import Flask, render_template, session, request, url_for, redirect, jsonify
+from flask import Flask, render_template, session, request, url_for, redirect, jsonify, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = 'dont ask about the key'
 
 # Check for environment variable
 if not "postgres://bezoopliubauew:ff447efa87a96336c27fa8c83def232d790d8b48f8da38e236259b4f447926f7@ec2-46-137-84-140.eu-west-1.compute.amazonaws.com:5432/d9kk3aqpv4nur1":
@@ -21,9 +23,11 @@ Session(app)
 engine = create_engine("postgres://bezoopliubauew:ff447efa87a96336c27fa8c83def232d790d8b48f8da38e236259b4f447926f7@ec2-46-137-84-140.eu-west-1.compute.amazonaws.com:5432/d9kk3aqpv4nur1")
 db = scoped_session(sessionmaker(bind=engine))
 
+
 @app.route("/")
 def index():
 	return render_template("index.html")
+
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
@@ -31,6 +35,7 @@ def register():
 		return render_template("error.html", mesasge = "Try registering!!")
 	else:
 		return render_template("register.html")
+
 
 @app.route("/register_user", methods=["POST", "GET"])
 def register_user():
@@ -50,13 +55,14 @@ def register_user():
 	db.execute("INSERT INTO user_details (username, password) VALUES (:username, :password)", {"username": user_name, "password": user_pass})
 	db.commit()
 
-	books = db.execute("SELECT * FROM books FETCH FIRST 15 ROW ONLY")
-	return render_template("homepage.html", books=books);
+	return render_template("success.html", message="Successfully Registered Now you can login")
+
 
 """Login to the page with username and password"""
 @app.route("/login")
 def login():
 	return render_template("index.html")	
+
 
 """If user exists create a local session for the user"""
 @app.route("/session", methods=["POST", "GET"])
@@ -70,40 +76,101 @@ def session():
 	if db.execute("SELECT * FROM user_details WHERE (username = :username AND password = :password)", {"username": user_name, "password": user_pass}).rowcount == 0:
 		return render_template("error.html", message="Invalid username or password! It takes only 15s to register! Try registering.");
 
-	"""Create a session for the user"""
-	books = db.execute("SELECT * FROM books FETCH FIRST 15 ROW ONLY")
-	return render_template("homepage.html", books=books)
+	else:
+		"""Create a session for the user"""
+		books = db.execute("SELECT * FROM books FETCH FIRST 15 ROW ONLY")
+		return render_template("homepage.html", books=books)
+
 
 @app.route("/homepage")
 def homepage():
 	books = db.execute("SELECT * FROM books FETCH FIRST 15 ROW ONLY")
 	return render_template("homepage.html", books = books)
 
+
+# Search books that match the string entered
 @app.route("/search", methods=["POST", "GET"])
 def search(): 	
 	# Get form information.
+	
 	try :
 		book_details = str(request.form.get("book_details"))
 	except ValueError:
 		return render_template("error.html", message="No book matches given details number!")
 
+	books = []
 	#Check for existence of book with provided title
-	if db.execute("SELECT * FROM books WHERE title = :title", {"title": book_details}).rowcount == 1:
-		book = db.execute("SELECT * FROM books WHERE title = :title", {"title": book_details}).fetchone()
-		return render_template("book.html", book=book)
+	# if db.execute("SELECT * FROM books WHERE title = :title", {"title": book_details}).rowcount >= 1:
+	title_book_isbn = db.execute("SELECT isbn FROM books WHERE title LIKE :title", {"title": '%' + str(book_details) + '%'}).fetchall()
+	if title_book_isbn != None:
+		for book in title_book_isbn:
+			books.append(book.isbn)
+		# return render_template("book.html", book=book)
 
 	#Check for existence of book with provided author name
-	if db.execute("SELECT * FROM books WHERE author = :author", {"author": book_details}).rowcount == 1:
-		book = db.execute("SELECT * FROM books WHERE author = :author", {"author": book_details}).fetchone()
-		return render_template("book.html", book=book)
+	# if db.execute("SELECT * FROM books WHERE author = :author", {"author": book_details}).rowcount >= 1:
+	author_book_isbn = db.execute("SELECT isbn FROM books WHERE author LIKE :author", {"author": '%' + str(book_details) + '%'}).fetchall()
+	if author_book_isbn != None:
+		for book in author_book_isbn:
+			books.append(book.isbn) 
+		#return render_template("book.html", book=book)
 
 	#Check for existence of book with provided isbn number: 3
-	if db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_details}).rowcount == 1:
-		book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_details}).fetchone()
-		return render_template("book.html", book=book)
-
-	return render_template("error.html", message="Try entering correct details!")
+	# if db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_details}).rowcount >= 1:
+	isbn_book_isbn = db.execute("SELECT isbn FROM books WHERE isbn LIKE :isbn", {"isbn": '%' + str(book_details) + '%'}).fetchall()
+	if isbn_book_isbn != None:
+		for book in isbn_book_isbn:
+			books.append(book.isbn)
+		# return render_template("book.html", books=book)
+	
+	if not books:
+		return render_template("error.html", message="Try entering correct details!")
+	
+	else:
+		for i in range(0,len(books)):
+			books[i] = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": books[i]}).fetchone()
+		return render_template("search.html", books=books)
 	#Corresponding error messages
+
+
+# Details about about with provided isbn
+@app.route("/book/<string:details>")
+def book(details):
+	if details is None:
+		return render_template("error.html", message="URL not found!!")
+	book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": details}).fetchone()
+
+	if book is None:
+		return render_template("error.html", message = "No book with entered title / Invalid ISBN")
+	
+	return render_template("book.html", book=book)
+
+"""Avg Ratings and rating distribution, total count"""
+@app.route("/api/<string:title>", methods=["POST", "GET"])
+def book_api(title):
+	res = requests.get("https://www.goodreads.com/book/title.json", 
+						params={"format": json, "key": "P2LGOZuBDHH7LSzJZhnsA", "title": "Blink"})
+	if res.status_code != 200:
+		raise Exception("Error: API request failed!")
+	details = res.json()
+
+"""Bookmarked Books"""
+@app.route("/marked")
+def marked():
+	book_isbn = db.execute("SELECT *")
+	books = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_isbn}).fetchall()
+	return render_template("marked.html", books=books)
+
+"""Print details of all the books in database"""
+@app.route("/books/<string:details>")
+def books(details):
+	books = db.execute("SELECT * FROM books WHERE isbn OR title OR author LIKE '%a%'").fetchall()
+	return render_template("books.html", books=books)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
 """ if error_type == 1:
 		return render_template("error.html", message="Try entering correct details!")
 	
@@ -116,30 +183,3 @@ def search():
 	else:
 		return render_template("error.html", message="Invalid ISBN number!")	
 """
-@app.route("/book/<string:details>")
-def book(details):
-	if details is None:
-		return render_template("error.html", message="URL not found!!")
-	book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_isbn}).fetchone()
-
-	if book is None:
-		return render_template("error.html", message = "No book with entered title / Invalid ISBN")
-	
-	return render_template("book.html", book=book)
-
-"""Print details of all the books in database"""
-@app.route("/books/<string:details>")
-def books(details):
-	books = db.execute("SELECT * FROM books WHERE isbn OR title OR author LIKE '%a%'").fetchall()
-	return render_template("books.html", books=books)
-
-@app.route("/api/<string:title>", methods=["POST", "GET"])
-def book_api(title):
-	res = requests.get("https://www.goodreads.com/book/title.json", 
-							params={"format": json, "key": "P2LGOZuBDHH7LSzJZhnsA", "title": "Blink"})
-	if res.status_code != 200:
-		raise Exception("Error: API request failed!")
-	details = res.json()
-
-if __name__ == '__main__':
-	main()
